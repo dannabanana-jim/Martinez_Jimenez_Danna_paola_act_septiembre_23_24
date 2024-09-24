cdc434dc2a37cc131c76b957dd5706749d5e718f
Danna Jim <a.23308051281195@cbtis128.edu.mx>
2024-09-23 20:20:31 -0600

    Add files via upload

---
+++ pr7_if_else.py
@@ -0,0 +1,33 @@
+#Practica de la funcion 
+#if else
+
+"""
+print("Practicando if else")
+
+x = 200
+y = 1000
+z = 1500
+
+if (x == 200):
+    print  ("x = 200")
+else:
+    print ("x > y")
+"""
+
+#Ejemplo de IF else anidado
+
+print("Ejemplo de If Else Anidado")
+
+z = 0
+
+if z > 8:
+    print ("¡No voy a imprimir!") #esta sentencia no se ejecuta
+elif z > 5:
+    print("¡Yo lo hare!") #Esta tendencia se ejecuta
+elif z > 6:
+    print("¡Tampoco voy a imprimir!") #esta sentencia no se ejecuta
+else:
+    print("¡Yo tampoco!") #esta sentencia no se ejecuta
+
+
+
